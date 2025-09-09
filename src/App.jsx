a97d8284d4a7570967a8301b7656eec58ac05624
pablo <pabloccsanchez@gmail.com>
2025-09-09 18:35:23 -0500

    Crear una forma de hacer SPAs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  // cambia la URL pero no refresca la página
+  window.history.pushState({}, '', href)
+  // evento para avisar que URL cambio
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage () {
   return (
     <>
       <h1>Home</h1>
       <p>Página de inicio.</p>
-      <a href='/about'>Ir a Sobre nosotros</a>
+      <button onClick={() => navigate('/about')}>Ir a Sobre nosotros</button>
     </>
   )
 }
@@ -15,13 +25,25 @@ function AboutPage () {
     <>
       <h1>About</h1>
       <p>Página sobre el about.</p>
-      <a href='/'>Ir a la Home</a>
+      <button onClick={() => navigate('/')}>Ir a la Home</button>
     </>
   )
 }
 
 function App () {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
 
   return (
     <main>
